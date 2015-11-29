448c4e6716b4ed32caa7ac290463b53d1340b8a4
fastcode developer leader <m.ortegaplaza@gmail.com>
2015-11-29 12:53:12 +0100

    Desarrollando y Configurando la Vista y el Controlador

--- SPRING_base_project_MVC_JPA/src/main/java/com/companyname/springapp/web/HelloController.java
+++ SPRING_base_project_MVC_JPA/src/main/java/com/companyname/springapp/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.companyname.springapp.web;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -21,8 +22,9 @@ public class HelloController {
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
-        logger.info("Returning hello view");
+    	 String now = (new Date()).toString();
+         logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("hello.jsp");
+         return new ModelAndView("hello", "now", now);
     }
 }
